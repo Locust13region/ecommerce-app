0c1b7f075add72c45aaf9927dcffee1f33adc1b8
DESKTOP-MF8V5D1\Andrey <m>
2025-05-05 15:00:24 +0300

    TASK-26 refactor: change theme to Material

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+import Material from '@primeuix/themes/material'
 
 import App from './App.vue'
 import router from './router'
@@ -12,7 +12,7 @@ const app = createApp(App)
 
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: Material,
     options: {
       prefix: 'p',
       darkModeSelector: 'system',
