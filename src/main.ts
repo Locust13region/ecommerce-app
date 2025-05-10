ea5b00a7f93bcc157b31a1cacf6ca3c5c6a8bb56
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-05-10 19:14:34 +0400

    feat: configure theme for project

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,41 @@
+import 'primeicons/primeicons.css'
+import './assets/base.css'
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Material from '@primeuix/themes/material'
-
 import App from './App.vue'
 import router from './router'
+import { definePreset } from '@primeuix/themes'
 
 const app = createApp(App)
+const MainPreset = definePreset(Material, {
+  semantic: {
+    primary: {
+      50: '{indigo.50}',
+      100: '{indigo.100}',
+      200: '{indigo.200}',
+      300: '{indigo.300}',
+      400: '{indigo.400}',
+      500: '{indigo.500}',
+      600: '{indigo.600}',
+      700: '{indigo.700}',
+      800: '{indigo.800}',
+      900: '{indigo.900}',
+      950: '{indigo.950}',
+    },
+  },
+})
 
 app.use(PrimeVue, {
   theme: {
-    preset: Material,
+    preset: MainPreset,
     options: {
       prefix: 'p',
-      darkModeSelector: 'system',
-      cssLayer: false,
     },
   },
 })
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
