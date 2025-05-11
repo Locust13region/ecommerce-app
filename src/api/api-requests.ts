40ad3963706a8e12363da5feb303b8e80817888f
DESKTOP-MF8V5D1\Andrey <m>
2025-05-11 12:23:20 +0300

    fix: script name, remove unnecessary file

--- src/api/api-requests.ts
+++
@@ -1,6 +0,0 @@
-import { apiRoot } from './build-client'
-
-export async function getCustomerById(id: string) {
-  const response = await apiRoot.customers().withId({ ID: id }).get().execute()
-  return response.body
-}
