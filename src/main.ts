cbebfc8be5b3235048241df65238102bda76f5c3
Abramova Elizaveta <a.d.elizaveta@gmail.com>
2025-05-17 12:38:24 +0400

    refactor: move theme preset to separate file

--- src/main.ts
+++ src/main.ts
@@ -4,39 +4,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-import Material from '@primeuix/themes/material'
+import { MainPreset } from './composables/preset'
 import App from './App.vue'
 import router from './router'
-import { definePreset } from '@primeuix/themes'
 import ToastService from 'primevue/toastservice'
 
 const app = createApp(App)
-const MainPreset = definePreset(Material, {
-  semantic: {
-    primary: {
-      50: '{purple.50}',
-      100: '{purple.100}',
-      200: '{purple.200}',
-      300: '{purple.300}',
-      400: '{purple.400}',
-      500: '{purple.500}',
-      600: '{purple.600}',
-      700: '{purple.700}',
-      800: '{purple.800}',
-      900: '{purple.900}',
-      950: '{purple.950}',
-    },
-  },
-})
-
-app.use(PrimeVue, {
-  theme: {
-    preset: MainPreset,
-    options: {
-      prefix: 'p',
-    },
-  },
-})
 
 router.beforeEach(async (to /*, from*/) => {
   //TODO: replace isLogined with state
@@ -46,6 +19,15 @@ router.beforeEach(async (to /*, from*/) => {
       name: 'home',
     }
   }
+})
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MainPreset,
+    options: {
+      prefix: 'p',
+    },
+  },
 })
 
 app.use(createPinia())
