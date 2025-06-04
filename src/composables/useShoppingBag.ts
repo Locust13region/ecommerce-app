e074534a3428dc641dbb25e16e6a22aa35c442c0
DESKTOP-MF8V5D1\Andrey <m>
2025-06-05 00:14:34 +0300

    refactor: composable

--- src/composables/useShoppingBag.ts
+++ src/composables/useShoppingBag.ts
@@ -7,68 +7,127 @@ export function useShoppingBag() {
   const bagId = ref('')
   const bagVersion = ref(0)
 
-  const getBag = async () => {
-    const cartsResponse = await api.root.me().carts().get().execute()
-    const carts = cartsResponse.body.results
-    const activeCart = carts.find((cart) => cart.cartState === 'Active')
-    if (activeCart) {
-      bagId.value = activeCart.id
-      bagVersion.value = activeCart.version
-    } else {
-      const createResponse = await api.root
-        .me()
-        .carts()
-        .post({
-          body: {
-            currency: 'USD',
-            country: 'US',
-          },
-        })
-        .execute()
-      const newCart = createResponse.body
-      bagId.value = newCart.id
-      bagVersion.value = newCart.version
+  const getBag = async (): Promise<{ id: string; version: number }> => {
+    try {
+      const cartsResponse = await api.root.me().carts().get().execute()
+      const carts = cartsResponse.body.results
+      const activeCart = carts.find((cart) => cart.cartState === 'Active')
+
+      if (activeCart) {
+        bagId.value = activeCart.id
+        bagVersion.value = activeCart.version
+      } else {
+        const createResponse = await api.root
+          .me()
+          .carts()
+          .post({
+            body: {
+              currency: 'USD',
+              country: 'US',
+            },
+          })
+          .execute()
+
+        const newCart = createResponse.body
+        bagId.value = newCart.id
+        bagVersion.value = newCart.version
+      }
+
+      return { id: bagId.value, version: bagVersion.value }
+    } catch (error) {
+      console.error('Failed to get or create cart:', error)
+      throw error //TODO реализовать перехват ошибок на уровне выше
     }
   }
 
   const getBagId = () => bagId.value
   const getBagVersion = () => bagVersion.value
 
-  return { getBag, getBagId, getBagVersion, addToBag, removeFromBag }
+  const addToBag = async ({ sku, quantity = 1 }: { sku: string; quantity?: number }) => {
+    try {
+      const { id, version } = await getBag()
+
+      const response = await api.root
+        .me()
+        .carts()
+        .withId({ ID: id })
+        .post({
+          body: {
+            version,
+            actions: [
+              {
+                action: 'addLineItem',
+                sku,
+                quantity,
+              },
+            ],
+          },
+        })
+        .execute()
+
+      bagVersion.value = response.body.version
+    } catch (error) {
+      if (
+        typeof error === 'object' &&
+        error !== null &&
+        'statusCode' in error &&
+        error.statusCode === 409
+      ) {
+        console.warn('Cart version conflict. Retrying...')
+        return addToBag({ sku, quantity })
+      } else {
+        console.error('Failed to add to cart by SKU:', error)
+      }
+    }
+  }
+
+  const removeFromBag = async (lineItemId: string) => {
+    try {
+      const { id, version } = await getBag()
+
+      const response = await api.root
+        .me()
+        .carts()
+        .withId({ ID: id })
+        .post({
+          body: {
+            version,
+            actions: [
+              {
+                action: 'removeLineItem',
+                lineItemId,
+              },
+            ],
+          },
+        })
+        .execute()
+
+      bagVersion.value = response.body.version
+    } catch (error) {
+      if (
+        typeof error === 'object' &&
+        error !== null &&
+        'statusCode' in error &&
+        error.statusCode === 409
+      ) {
+        console.warn('Cart version conflict. Retrying...')
+        return removeFromBag(lineItemId)
+      } else {
+        console.error('Failed to remove from cart:', error)
+      }
+    }
+  }
+
+  const getAllBags = async () => {
+    await api.root.me().carts().get().execute()
+  } // техническая функция
+
+  return {
+    getBag,
+    getAllBags,
+    getBagId,
+    getBagVersion,
+    addToBag,
+    removeFromBag,
+  }
 }
-// await getApiRoot().me().carts()
-//     .post({
-//         body: {
-//             currency: "USD",
-//             country: "US",
-//             customerEmail: "a-g@proton.com",
-//         },
-//     })
-//     .execute()
-
-// await getApiRoot().me().carts().withId({ ID: "c7d9ebe4-54e9-43a4-ad9d-452ec1857a66" }).delete({
-//     queryArgs: {
-//         version: 1,
-//     },
-// }).execute();
-
-// await getApiRoot().carts()
-//     .withId({ ID: "c7d9ebe4-54e9-43a4-ad9d-452ec1857a66" })
-//     .post({
-//         body: {
-//             version: 16,
-//             actions: [
-//                 {
-//                     action: 'addLineItem',
-//                     sku: "01-001",
-//                     quantity: 1,
-//                 }
-//             ]
-//         }
-//     })
-//     .execute();
-
-// await getApiRoot().products()
-//     .withId({ ID: 'b92dc2f8-f330-49f0-b2be-50aa3e904281' })
-//     .get()
-//     .execute();
